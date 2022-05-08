3f6edd2090d54eb184907ab6ea312de0cade0231
Zhiyu-Hu <52404474+Zhiyu-Hu@users.noreply.github.com>
2022-05-08 23:35:20 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 export default {
-  title: 'yun yu yuan', // 网站标题
-  nickName: "云与原", // 昵称
-  githubName: 'yunyuyuan', // github账户名
+    title: 'QiDian', // 网站标题
+  nickName: "奇点", // 昵称
+  githubName: 'Zhiyu-Hu', // github账户名
   githubRepo: 'nuxt2-blog', // 勿修改
   githubBranch: 'master', // 勿修改
   domain: 'https://blog.halberd.cn', // rss域名
-  SEO_description: '云与原的博客，书写值得书写的东西',
-  SEO_keywords: 'yunyuyuan,yunyuyuan\'s blog,云与原,云与原的博客',
+  SEO_description: '奇点的博客，书写值得书写的东西',
+  SEO_keywords: 'QiDian,QiDian\'s blog,奇点,奇点的博客',
   CloudflareAnalyze: "92cdf13c9c964ad1ab71c638c6fde0fc", // cloudflare的webanalyze
 }
